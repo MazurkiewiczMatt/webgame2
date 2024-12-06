13203c6116990b254c23ceab44530fb7abb8e6f2
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-12-06 15:54:17 +0100

    bugfix

--- server/app.py
+++ server/app.py
@@ -1,6 +1,10 @@
 # server.py
 from flask import Flask, render_template
-import authentication, characters_management
+
+from .authentication import signup, login, logout
+from .characters_management import character_sheet
+
+from state_management import load_users
 
 
 def create_app(active_sessions=None, characters=None):
@@ -12,24 +16,24 @@ def create_app(active_sessions=None, characters=None):
     @app.route('/')
     def main_app():
         active_players = list(active_sessions.keys())
-        registered_users = list(authentication.load_users().keys())
+        registered_users = list(load_users().keys())
         return render_template('main_page.html', active_players=active_players, registered_users=registered_users)
 
     @app.route('/signup', methods=['POST'])
     def signup():
-        return authentication.signup()
+        return signup()
 
     @app.route('/login', methods=['POST'])
     def login():
-        return authentication.login(active_sessions)
+        return login(active_sessions)
 
     @app.route('/logout', methods=['POST'])
     def logout():
-        return authentication.logout(active_sessions)
+        return logout(active_sessions)
 
     @app.route('/character_sheet', methods=['POST'])
     def character_sheet():
-        return characters_management.character_sheet(active_sessions, characters)
+        return character_sheet(active_sessions, characters)
 
     return app
 
