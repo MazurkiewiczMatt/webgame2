3c98fb19936e6d0bf870f3e0201d4e1ecc5d1e21
MazurkiewiczMatt <mateusz.a.mazurkiewicz@gmail.com>
2024-12-16 23:54:08 +0100

    change file

--- server_app/app.py
+++ server_app/app.py
@@ -1,25 +1,11 @@
 from flask import Flask, render_template
-from apscheduler.schedulers.background import BackgroundScheduler
 from .authentication import signup, login, logout
 from .characters_management import character_sheet, create_character, load_characters, save_characters
-
-def update_function():
-    characters = load_characters()
-    for character in characters.keys():
-        if "age" in characters[character].keys():
-            characters[character]["age"] += 1
-        else:
-            characters[character]["age"] = 1
-    save_characters(characters)
 
 # Flask app creation
 def create_app(registered_users=None, active_sessions=None):
     app = Flask(__name__, template_folder='/home/Mazurkiewicz/webgame/webgame2/templates/')
 
-    # Initialize the scheduler
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(update_function, 'cron', hour="0-23", minute="*")  # Run every minute
-    scheduler.start()
 
     @app.route('/')
     def main_app():
@@ -46,15 +32,5 @@ def create_app(registered_users=None, active_sessions=None):
     @app.route('/create_character', methods=['POST'])
     def create_character_endpoint():
         return create_character(active_sessions)
-
-    # Ensure scheduler shuts down when the app is stopped
-    @app.before_first_request
-    def start_scheduler():
-        if not scheduler.running:
-            scheduler.start()
-
-    @app.teardown_appcontext
-    def shutdown_scheduler(exception=None):
-        scheduler.shutdown(wait=False)
 
     return app
